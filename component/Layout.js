20da86ff432cbe95d4f04063f5601ba8ea4128bd
Brendon <Brendonvandernest69@gmail.com>
2021-06-21 19:54:08 +0200

    fix information on the about page

--- component/Layout.js
+++ component/Layout.js
@@ -1,51 +1,51 @@
-import Nav from '../component/Nav';
-import Head from 'next/head';
+import Nav from "../component/Nav";
+import Head from "next/head";
 
 // set layout for all the pages that are being render.
-const Layout = ({children}) => {
-    return (
-        <>
-            <Head>
-                <link   
-                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
-                        rel="stylesheet" 
-                        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" 
-                        crossorigin="anonymous" 
-                />
-            </Head>
-            <Nav />
-            <div className='container-fluid background'>
-                <p>{children}</p>
-            </div>
-            <footer>
-                <p>This site has been built with the use of nextjs.</p>
-            </footer>
-            <script 
-                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" 
-                integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" 
-                crossorigin="anonymous"
-            />
-            <style jsx>{`
-                .background {
-                    background-color: #171717;
-                    height: 100%;
-                    min-height: 100vh;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: flex-start;
-                }
-                footer {
-                    background-color: #171717;
-                    display: flex;
-                    justify-content: center;
-                    align-content: flex-end;
-                }
-                footer p {
-                    color: #444444; 
-                }
-            `}</style>
-        </>
-    );
-}
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+          crossorigin="anonymous"
+        />
+      </Head>
+      <Nav />
+      <div className="container-fluid background">
+        <p>{children}</p>
+      </div>
+      <footer>
+        <p>This site has been built with the use of nextjs.</p>
+      </footer>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossorigin="anonymous"
+      />
+      <style jsx>{`
+        .background {
+          background-color: #171717;
+          height: 100%;
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+        }
+        footer {
+          background-color: #171717;
+          display: flex;
+          justify-content: center;
+          align-content: flex-end;
+        }
+        footer p {
+          color: #444444;
+        }
+      `}</style>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
--- pages/about.js
+++ pages/about.js
@@ -1,60 +1,67 @@
 // showcasing my exprience and education with the use of jsx styling.
 const about = () => {
-    return (
-        <>
-            <h1>Experience</h1>
-            <div>
-                <p>Company: Orderwise</p>
-                <p>Position: Support Engineer</p>
-                <p>Location: Gauteng, Standton</p>
-                <p>Start: Oct - 2019 End-date: Present</p>
-                <p>Duty:  Providing customer and technical support for the software that is being built by Orderwise.</p>
-            </div>
-            <div>
-                <p>Company: True Technoligies Inc</p>
-                <p>Position: Workshop Technician</p>
-                <p>Location: Eastern Cape, Jeffreys-bay</p>
-                <p>Start: Apr 2019 End-date: Sep 2019</p>
-                <p>Duty: Form repairing customers computers/laptop to throubleshooting the ISP faults.</p>
-            </div>
-            <div>
-                <p>Company: Digital Horizon</p>
-                <p>Position: Field Technician</p>
-                <p>Location: Gauteng, Benoni</p>
-                <p>Start: Feb 2018 End-date: Oct 2018</p>
-                <p>Duty: Assisted with all remote enquiries and onsite flauts.</p>
-            </div>
-            <h1>Education</h1>
-            <div>
-                <p>Company: Digital Horizon</p>
-                <p>Position: Field Technician</p>
-                <p>Location: Gauteng, Benoni</p>
-                <p>Start: Feb 2018 End-date: Oct 2018</p>
-                <p>Duty: Assisted with all remote enquiries and onsite flauts.</p>
-            </div>
-            <style jsx>{`
-                h1 , h2 , p {
-                color: #EDEDED;
-                }
-                div {
-                    margin: 1%;
-                    border: 5px solid black;
-                    border-radius: 25px;
-                    background-color: #444444;
-                }
-                div p {
-                    display: flex;
-                    justify-content: flex-start;
-                    margin-left: 5%;
-                    margin-right: auto;
-                }
-                p {
-                    color: #EDEDED;
-                    font-weight: bold;
-                }
-            `}</style>
-        </>
-    );
-}
+  return (
+    <>
+      <h1>Experience</h1>
+      <div>
+        <p>Company: Orderwise</p>
+        <p>Position: Support Engineer</p>
+        <p>Location: Gauteng, Standton</p>
+        <p>Start: Oct - 2019 End-date: Present</p>
+        <p>
+          Duty: Providing customer and technical support for the software that
+          is being built by Orderwise.
+        </p>
+      </div>
+      <div>
+        <p>Company: True Technoligies Inc</p>
+        <p>Position: Workshop Technician</p>
+        <p>Location: Eastern Cape, Jeffreys-bay</p>
+        <p>Start: Apr 2019 End-date: Sep 2019</p>
+        <p>
+          Duty: Form repairing customers computers/laptop to throubleshooting
+          the ISP faults.
+        </p>
+      </div>
+      <div>
+        <p>Company: Digital Horizon</p>
+        <p>Position: Field Technician</p>
+        <p>Location: Gauteng, Benoni</p>
+        <p>Start: Feb 2018 End-date: Oct 2018</p>
+        <p>Duty: Assisted with all remote enquiries and onsite flauts.</p>
+      </div>
+      <h1>Education</h1>
+      <div>
+        <p>Institution: HyperionDev</p>
+        <p>Qualification: Certification</p>
+        <p>Start date: 11 Jan 2021 End-date: 11 Jan 2021</p>
+        <p>Course: Full stack web development</p>
+      </div>
+      <style jsx>{`
+        h1,
+        h2,
+        p {
+          color: #ededed;
+        }
+        div {
+          margin: 1%;
+          border: 5px solid black;
+          border-radius: 25px;
+          background-color: #444444;
+        }
+        div p {
+          display: flex;
+          justify-content: flex-start;
+          margin-left: 5%;
+          margin-right: auto;
+        }
+        p {
+          color: #ededed;
+          font-weight: bold;
+        }
+      `}</style>
+    </>
+  );
+};
 
-export default about
+export default about;
