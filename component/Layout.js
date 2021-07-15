1af78c20ff5fa71529766425860961bb468d79c4
Brendon <Brendonvandernest69@gmail.com>
2021-07-15 21:43:16 +0200

    remove tracking code

--- component/Layout.js
+++ component/Layout.js
@@ -12,14 +12,6 @@ const Layout = ({ children }) => {
           integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
           crossorigin="anonymous"
         />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NQHYSPZ6KC"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-NQHYSPZ6KC');
-        </script>
       </Head>
       <Nav />
       <div className="container-fluid background">
